b4fcbdcbd3428db02a59563e273370fc71160ed7
ozwaldorf <self@ossian.dev>
2024-11-05 16:26:04 -0500

    dnscrypt to prevent isp fuckery, other updates

--- system/configuration.nix
+++ system/configuration.nix
@@ -44,11 +44,13 @@
 
   networking = {
     hostName = hostname;
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
     nameservers = [
-      # "9.9.9.9" # quad9
-      # "1.1.1.1" # cloudflare
-      "8.8.8.8" # google
+      "127.0.0.1"
+      "::1"
     ];
     hosts = {
       "127.0.0.1" = [
@@ -84,6 +86,32 @@
       ];
     };
 
+  };
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+      server_names = [
+        "cloudflare"
+        "cloudflare-ipv6"
+        "google"
+        "google-ipv6"
+      ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 
   time.timeZone = "America/New_York";
@@ -242,6 +270,8 @@
   fonts.fontconfig.enable = true;
 
   programs = {
+    steam.enable = true;
+
     # enable installing zsh at the system level to set the users default terminal. Everything else configuration wise is done in home manager.
     zsh = {
       enable = true;
